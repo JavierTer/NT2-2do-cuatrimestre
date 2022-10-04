1e012dc80e525768a97bbb45b503861193179455
Javi <javier.t.sartore@gmail.com>
2022-10-03 21:30:37 -0300

    agrego botones

--- clase5 consultas apis/ejercicioTarea/app.js
+++ clase5 consultas apis/ejercicioTarea/app.js
@@ -1,27 +1,59 @@
 console.log('Ejercicios de consulta a apis');
 
-const aplication = new Vue ({
-    el:"#app",
+const aplication = new Vue({
+    el: "#app",
     data() {
         return {
-            texto:"Cripto ranking",
+            texto: "Cripto ranking",
             monedas: [],
         };
     },
     async created() {
         console.log('created...');
         const limit = 10;
-        const resultado = await fetch (
+
+        const resultado = await fetch(
             `https://api.coincap.io/v2/assets?limit=${limit}`);
+
         const data = await resultado.json();
         //console.log(data);
-        this.monedas = data.data;    
+        this.monedas = data.data;
         console.log(this.monedas);
     },
     methods: {
         ordenarPorNombre() {
             //el this hace referencia a la instancia de vue.
-            this.texto = 'probando';
+           return this.monedas.sort((a ,b) => a.name.localeCompare(b.name))
+            
+        },
+        ordenarPorRanking() {
+
+            return this.monedas.sort((a, b) => a.rank - b.rank)
+        },
+        verificarHs(){
+            let estado
+            let arrayEstado = [];
+            for (let i = 0; i < this.monedas.length; i++) {
+                if(monedas[i].changePercent24Hr > 2) {
+                    estado = '⬆'
+                    arrayEstado.push(estado)
+                }
+                else{
+                    arrayEstado.push(estado)
+                }
+            
+            }
+            return arrayEstado
         }
-    
+    }
+    /*
+    computed: {
+        ordenarPorRanking() {
+
+            return this.monedas.sort((a, b) => a.rank - b.rank)
+        }
+    }
+    */
 })
+
+//
